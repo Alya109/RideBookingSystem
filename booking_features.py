dd9f3c9898fcdf7bc4f48579186b019f52b30e96
Alya <149942623+Alya109@users.noreply.github.com>
2025-06-16 09:43:59 +0800

    Update booking_features.py

---
+++ booking_features.py
@@ -0,0 +1,18 @@
+from vehicle import Van, Car, Motorcycle
+
+class Booking:
+    def __init__(self, user, vehicle, start_location, end_location, distance):
+        self.user = user
+        self.vehicle = vehicle
+        self.start_location = start_location
+        self.end_location = end_location
+        self.distance = distance
+        self.total_cost = 
+    def add_booking():
+
+    def check_all_booking():
+
+    def cancel_booking():
+        
+class ManageBooking():
+    
