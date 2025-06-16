aeea258913bb29c02901c3fa55d5c5f043f022c2
Alya <149942623+Alya109@users.noreply.github.com>
2025-06-16 09:18:55 +0800

    Update and rename vehicle_mgmt.py to vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod 
+
+class Vehicle(ABC):
+    def __init__(self, name, cost_per_mile, capacity) -> None:
+        sef.name = name
+        self._cost_per_mile = cost_per_mile
+        self._capacity = capacity
+    
+    @abstractmethod
+    def cost_calculation(self, distance) -> float:
+        pass
+        
+class Van(Vehicle):
+    def cost_calculation(self, distance) -> float:
+        return distance * self.cost_per_mile # Extra fee
+        
+class Car(Vehicle):
+    def cost_calculation(self, distance) -> float:
+        return distance * self.cost_per_mile + 10 # Extra fee
+        
+class Motorcycle(Vehicle):
+    def cost_calculation(self, distance) -> float:
+        return distance * self.cost_per_mile # Base cost fee // only 1 capacity
+
