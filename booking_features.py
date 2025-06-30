2e7faaeaab6d6d3784bb77bbcb98411c9d0da485
Alya <earllumabi7000@gmail.com>
2025-07-01 03:48:32 +0800

    Add

--- booking_features.py
+++ booking_features.py
@@ -1,14 +1,10 @@
-from vehicle import Van, Car, Motorcycle
+from Vehicle import Motorcycle, Taxi, Car, ElectricCar, Van
 
-class Booking:
-    def __init__(self, user, vehicle, start_location, end_location, distance):
-        self.user = user
-        self.vehicle = vehicle
-        self.start_location = start_location
-        self.end_location = end_location
-        self.distance = distance
-        self.total_cost = 0
-
-class ManageBooking():
-    def add_booking():
+class BookingFeatures:
+    def __init__(self):
+        self.motorcycle = Motorcycle()
+        self.taxi = Taxi()
+        self.car = Car()
+        self.electric_car = ElectricCar()
+        self.van = Van()
         
--- vehicle.py
+++ vehicle.py
@@ -2,41 +2,72 @@ from abc import ABC, abstractmethod
 
 """ Parent class for all vehicle types """
 class Vehicle(ABC):
-    def __init__(self, name, base_fee, cost_per_mile, capacity) -> None:
+    def __init__(self, name, base_fee, cost_per_mile, capacity, luxury_fee=0, rate_per_minute=0) -> None:
         self.name = name
         self._base_fee = base_fee
         self._cost_per_mile = cost_per_mile
         self._capacity = capacity
-    
-    def capacity(self) -> int:
-        return self._capacity
-    
-    def vehicle_type(self) -> str:
-        return self.__class__.__name__
-    
-    def base_rate(self) -> float:
-        return self._base_fee
+        self._luxury_fee = luxury_fee
+        self._rate_per_minute = rate_per_minute
     
     @abstractmethod
     def calculate_cost(self) -> float:
         pass
 
-""" Child class vehicle types """     
+""" Child class vehicle types """   
+  
 class Motorcycle(Vehicle):
-    pass
+    
+    def __init__(self):
+        pass
+    
+    def calculate_cost(self, distance: float) -> float:
+        total_cost = self._base_fee + (distance * self._cost_per_mile)
+        return total_cost 
 
 class Taxi(Vehicle):
-    pass
+    
+    def __init__(self):
+        pass
+    
+    def calculate_cost(self):
+        pass
 
 class Car(Vehicle):
-    pass
+    
+    def __init__(self):
+        pass
+    
+    def calculate_cost(self, distance: float, time: float = 0) -> float:
+        rate_per_minute = self._rate_per_minute
+        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * rate_per_minute)
+        return total_cost
+        
 
 class ElectricCar(Vehicle):
-    pass
+    
+    def __init__(self):
+        pass
+    
+    def calculate_cost(self, distance: float, time: float = 0, luxury_fee: float = 0) -> float:
+        rate_per_minute = self._rate_per_minute
+        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * rate_per_minute) + luxury_fee
+        return total_cost
 
 class Van(Vehicle):
-    pass
-
+    
+    def __init__(self):
+        pass
+    
+    def calculate_cost(self, distance: float, time: float = 0, group_size: int = 1, luxury_fee: float = 0) -> float:
+        rate_per_minute = self._rate_per_minute
+        if group_size > 8:
+            group_size_fee = 50  # Additional fee for large groups
+        else:
+            group_size_fee = 30
+            
+        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * rate_per_minute) + group_size_fee + luxury_fee
+        return total_cost
 
 
 
