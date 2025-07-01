1a22ba862bf16adc91c90fa57c0b397ac6757fd9
Alya <earllumabi7000@gmail.com>
2025-07-01 17:58:29 +0800

    Vehicle base code

--- vehicle.py
+++ vehicle.py
@@ -18,8 +18,8 @@ class Vehicle(ABC):
   
 class Motorcycle(Vehicle):
     
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        super().__init__(name="Motorcycle", base_fee=60, cost_per_mile=10, capacity=1, luxury_fee=0, rate_per_minute=0)
     
     def calculate_cost(self, distance: float) -> float:
         total_cost = self._base_fee + (distance * self._cost_per_mile)
@@ -27,53 +27,42 @@ class Motorcycle(Vehicle):
 
 class Taxi(Vehicle):
     
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        super().__init__(name="Taxi", base_fee=80, cost_per_mile=12, capacity=4, luxury_fee=0, rate_per_minute=1.5)
     
-    def calculate_cost(self):
-        pass
-
+    def calculate_cost(self, distance: float, time: float = 0) -> float:
+        total_cost = self._base_fee + (distance * self._cost_per_mile)  # Assuming a fixed distance of 10 miles for simplicity
+        return total_cost
+    
 class Car(Vehicle):
-    
     def __init__(self, name: str) -> None:
         super().__init__(name, base_fee=100, cost_per_mile=14, capacity=4, rate_per_minute=2, luxury_fee=0)
     
     def calculate_cost(self, distance: float, time: float = 0) -> float:
-        rate_per_minute = self._rate_per_minute
-        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * rate_per_minute)
+        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * self._rate_per_minute)
         return total_cost
         
 
 class ElectricCar(Vehicle):
     
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        super().__init__(name="Electric Car", base_fee=80, cost_per_mile=14, capacity=4, rate_per_minute=2, luxury_fee=20)
     
-    def calculate_cost(self, distance: float, time: float = 0, luxury_fee: float = 0) -> float:
-        rate_per_minute = self._rate_per_minute
-        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * rate_per_minute) + luxury_fee
+    def calculate_cost(self, distance: float, time: float = 0) -> float:
+        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * self._rate_per_minute) + self._luxury_fee
         return total_cost
 
 class Van(Vehicle):
     
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        super().__init__(name="Van", base_fee=120, cost_per_mile=16, capacity=12, rate_per_minute=3, luxury_fee=60)
     
     def calculate_cost(self, distance: float, time: float = 0, group_size: int = 1, luxury_fee: float = 0) -> float:
-        rate_per_minute = self._rate_per_minute
-        if group_size > 8:
-            group_size_fee = 50  # Additional fee for large groups
-        else:
-            group_size_fee = 30
-            
-        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * rate_per_minute) + group_size_fee + luxury_fee
+        group_size_fee = 50 if group_size > 8 else 30
+        total_cost = self._base_fee + (distance * self._cost_per_mile) + (time * self._rate_per_minute) + group_size_fee + luxury_fee
         return total_cost
 
 
-
-
-# Test case     
-sedan = Car("Sedan")
-
-print(sedan.name)
-print(sedan.calculate_cost(10)) 
+# Test
+van = Van()
+print(van.calculate_cost(10, 30, group_size=9, luxury_fee=20))  # Example usage
