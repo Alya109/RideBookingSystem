1d4ecc3319a937b647bb213c25597e1f3b9935e3
Alya <earllumabi7000@gmail.com>
2025-07-01 17:02:52 +0800

    add

--- booking_features.py
+++ booking_features.py
@@ -1,10 +1,12 @@
 from vehicle import Motorcycle, Taxi, Car, ElectricCar, Van
 
-class BookingFeatures:
-    def __init__(self):
-        self.motorcycle = Motorcycle()
-        self.taxi = Taxi()
-        self.car = Car()
-        self.electric_car = ElectricCar()
-        self.van = Van()
-        
+class Booking:
+    booking_id_counter = 1000
+    def __init__(self, vehicle_type, user, pickoff_location, dropoff_location, fare):
+        self.booking_id = Booking.booking_id_counter
+        Booking.booking_id_counter += 1
+        self.vehicle_type = vehicle_type
+        self.user = user
+        self.pickoff_location = pickoff_location
+        self.dropoff_location = dropoff_location
+        self.fare = fare
