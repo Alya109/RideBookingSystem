256ad33bc4f387401bdf39a5bffac1d41e04c704
Alya <earllumabi7000@gmail.com>
2025-07-01 18:51:04 +0800

    simulation type sht

--- timesimulation.py
+++ timesimulation.py
@@ -1,7 +1,18 @@
 import random
-import pandas as pd
 
+vehicle_speeds = {
+    "Motorcycle": 40,     # miles/hour
+    "Taxi": 35,
+    "Car": 38,
+    "Electric Car": 34,
+    "Van": 30
+}
 
-def estimate_travel_time(distance: float, vehicle_type) -> int:
+def simulate_time(distance, vehicle_type):
     
-    pass
+    speed = vehicle_speeds.get(vehicle_type, 30)  # Default speed if not found
+    eta = (distance / speed)  * 60 # time in hours
+    
+    simulated_time = random.randint(int(eta_minutes * 0.9), int(eta_minutes * 1.1))
+    return simulated_time
+
